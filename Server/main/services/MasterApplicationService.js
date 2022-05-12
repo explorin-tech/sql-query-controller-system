c4c7bbe79e46e2f2e67d42da0441ec89d3c5af2b
Himanshu Gupta <himanshugupta@spectre-air.myfiosgateway.com>
2022-05-12 17:11:49 -0400

    file-structure: Revamped the queries/dao layer in serverside.

--- Server/main/services/MasterApplicationService.js
+++ Server/main/services/MasterApplicationService.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const MasterApplicationDao = require('../dao/MasterApplicationDao')
+const {MasterApplication} = require('../dao/MasterApplication')
 
 
 module.exports.deleteNewApplication = (req, res, next) => {
   //var startTime = new Date()
   const application_id = req.body.application_id;
-  pool.query(
-    MasterApplicationDao.DELETE_AN_APPLICATION, [application_id],
-    (q_err, q_res) => {
-      console.log(q_err, q_res)
-      //var endTime = new Date()
-      //log.info("method = {}, API = {}, values = {}, request = {}, response = {}, error = {}, cost = {}", 
+  var params = {
+    application_id: application_id
+  }
+  var response = MasterApplication.deleteApplication(params)
+  return response;
+  //log.info("method = {}, API = {}, values = {}, request = {}, response = {}, error = {}, cost = {}", 
       //  "deleteNewApplication", MasterApplicationDao.DELETE_AN_APPLICATION, );
-      // res = status, rows, 
-      return res.json(q_res.rows);
-    }
-  );
+      
 }
