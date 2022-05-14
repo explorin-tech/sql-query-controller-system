98720f9718172810cca0ff5631cff694146b76a3
jai2201 <jai_s@me.iitr.ac.in>
2022-05-14 18:45:32 +0530

    comment: Fix comment typo in route names.

--- Server/main/routes/application_screen_rights_mapping.router.js
+++ Server/main/routes/application_screen_rights_mapping.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const routeNames = require('../constants/route_names');
 const { ApplicationScreenRightsMappingService } = require('../services');
 
-// Get screen rights mapping for a user.
+// Get screen rights mappings for a user.
 router.get(routeNames.GET_ALL_SCREEN_RIGHTS_FOR_AN_USER, [
   ApplicationScreenRightsMappingService.GET_getAllScreenRightsMappingForAnUser,
 ]);
