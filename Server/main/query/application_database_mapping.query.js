c50b0a56d5cc0607aa53b20f51040287ef1c17e9
jai2201 <jai_s@me.iitr.ac.in>
2022-05-15 12:41:41 +0530

    server: Correct typo error in DBAM table.

--- Server/main/query/application_database_mapping.query.js
+++ Server/main/query/application_database_mapping.query.js
@@ -4,7 +4,7 @@ module.exports = Object.freeze({
     'SELECT * FROM "DataBaseApplicationMapping" ORDER BY "DBAM_AddedOn" DESC',
   GET_DATABASE_DETAILS:
     'SELECT * FROM "DataBaseApplicationMapping" WHERE "DBAM_ID" = $1',
-  ADD_DATABASE_MAPPING: `INSERT INTO "DataBaseApplicationMapping"("DBAM_MA_ID", "DBAM_MA_Name", "DBAM_DBName", "DMAM_DBT_ID", "DMAM_DBT_Name", "DBAM_DBConnectionString", "DBAM_DBPortNumber", "DBAM_DBHostName", "DBAM_DBUserName" ,"DBAM_DBPassword", "DBAM_AddedOn", "DBAM_AddedBy", "DBAM_UpdatedOn", "DBAM_UpdatedBy") VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9 , $10, NOW(), $11, NOW(), $12) ON CONFLICT DO NOTHING`,
-  EDIT_DATABASE_MAPPING: `UPDATE "DataBaseApplicationMapping" SET "DBAM_MA_ID" = $2 , "DBAM_MA_Name" = $3, "DBAM_DBName" = $4, "DMAM_DBT_ID"=$5, "DMAM_DBT_Name"=$6, "DBAM_DBConnectionString"= $7, "DBAM_DBPortNumber"=$8 , "DBAM_DBHostName"= $9, "DBAM_DBUserName"= $10 ,"DBAM_DBPassword" = $11, "DBAM_UpdatedOn"= NOW(), "DBAM_UpdatedBy"=$12 WHERE "DBAM_ID"=$1`,
+  ADD_DATABASE_MAPPING: `INSERT INTO "DataBaseApplicationMapping"("DBAM_MA_ID", "DBAM_MA_Name", "DBAM_DBName", "DBAM_DBT_ID", "DBAM_DBT_Name", "DBAM_DBConnectionString", "DBAM_DBPortNumber", "DBAM_DBHostName", "DBAM_DBUserName" ,"DBAM_DBPassword", "DBAM_AddedOn", "DBAM_AddedBy", "DBAM_UpdatedOn", "DBAM_UpdatedBy") VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9 , $10, NOW(), $11, NOW(), $12) ON CONFLICT DO NOTHING`,
+  EDIT_DATABASE_MAPPING: `UPDATE "DataBaseApplicationMapping" SET "DBAM_MA_ID" = $2 , "DBAM_MA_Name" = $3, "DBAM_DBName" = $4, "DBAM_DBT_ID"=$5, "DBAM_DBT_Name"=$6, "DBAM_DBConnectionString"= $7, "DBAM_DBPortNumber"=$8 , "DBAM_DBHostName"= $9, "DBAM_DBUserName"= $10 ,"DBAM_DBPassword" = $11, "DBAM_UpdatedOn"= NOW(), "DBAM_UpdatedBy"=$12 WHERE "DBAM_ID"=$1`,
   DELETE_DATABASE_MAPPING: `DELETE FROM "DataBaseApplicationMapping" WHERE "DBAM_ID" = $1`,
 });
