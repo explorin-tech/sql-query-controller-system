616e926c58b1548f851c54bb816c06b32c0c400e
jai2201 <jai_s@me.iitr.ac.in>
2022-05-15 10:38:23 +0530

    server: Fix typos in queries for adding application screens.

--- Server/main/query/application_screen.query.js
+++ Server/main/query/application_screen.query.js
@@ -2,7 +2,7 @@ module.exports = Object.freeze({
   SELECT_ALL_APPLICATION_SCREENS:
     'SELECT * FROM "ApplicationScreen" ORDER BY "AS_AddedOn" DESC',
   GET_SCREEN_DETAILS: 'SELECT * from "ApplicationScreen" WHERE "AS_ID" = $1',
-  INSERT_NEW_APPLICATION_SCREEN: `INSERT INTO "ApplicationScreen"("AS_NAME", "AS_AddedOn" , "AS_AddedBy", "AS_UpdatedOn", "AS_UpdatedBy") VALUES($1, NOW(), $2, NOW(), $3 ) ON CONFLICT DO NOTHING`,
-  EDIT_SCREEN_DETAILS: `UPDATE "ApplicationScreen" SET "AS_NAME" = $2, "AS_UpdatedOn"=NOW(), "AS_UpdatedBy"=$3 WHERE "AS_ID"= $1`,
+  INSERT_NEW_APPLICATION_SCREEN: `INSERT INTO "ApplicationScreen"("AS_Name", "AS_AddedOn" , "AS_AddedBy", "AS_UpdatedOn", "AS_UpdatedBy") VALUES($1, NOW(), $2, NOW(), $3 ) ON CONFLICT DO NOTHING`,
+  EDIT_SCREEN_DETAILS: `UPDATE "ApplicationScreen" SET "AS_Name" = $2, "AS_UpdatedOn"=NOW(), "AS_UpdatedBy"=$3 WHERE "AS_ID"= $1`,
   DELETE_SCREEN: 'DELETE FROM "ApplicationScreen" WHERE "AS_ID" = $1',
 });
