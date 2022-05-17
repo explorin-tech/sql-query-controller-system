686b743bdb2b512c5d3152542222d3347995895f
jai2201 <jai_s@me.iitr.ac.in>
2022-05-18 01:26:34 +0530

    client: Set up signIn UI screens.

--- Client/src/containers/SignIn.js
+++ Client/src/containers/SignIn.js
@@ -1,5 +1,24 @@
 import React, { Fragment } from 'react';
 
+import '../static/css/signIn.css';
+import Image from '../static/images/signIn.png';
+
 export default function SignIn() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="signin">
+        <div>
+          <img src={Image} alt="intro" />
+        </div>
+        <div>
+          <h5>Sign in</h5>
+          <form>
+            <input type="text" placeholder="Username" /> <br />
+            <input type="text" placeholder="Password" /> <br />
+            <button>Continue</button>
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
