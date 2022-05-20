ae4763dc7132111c31e703d470dc9f8148b1a1bd
jai2201 <jai_s@me.iitr.ac.in>
2022-05-20 19:45:18 +0530

    signin: Add proper functionality for sending request to signin API and fetch token.

--- Client/src/containers/SignIn.js
+++ Client/src/containers/SignIn.js
@@ -1,23 +1,42 @@
-import React, { Fragment, useState } from "react";
-import { signinUser } from "../apis/signInUser";
+import React, { Fragment, useState } from 'react';
 
-import "../static/css/signIn.css";
-import Image from "../static/images/signIn.png";
+import '../static/css/signIn.css';
+import Image from '../static/images/signIn.png';
+
+import { SIGNIN_URL } from '../utils/BackendUrls';
+
+import axios from 'axios';
 
 export default function SignIn() {
   const [values, setValues] = useState({
-    email_id: "",
-    password: "",
+    email_id: '',
+    password: '',
   });
+  const [error, setError] = useState('');
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, [name]: event.target.value });
   };
 
-  const handleEnter = (e) => {
-    if (e.key === "Enter") {
-      signinUser(values);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(SIGNIN_URL, {
+        user: {
+          email: values.email_id,
+          password: values.password,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+        }
+      })
+      .catch(function (error) {
+        if (error.response) {
+          setError(error.response.data.message);
+        }
+      });
   };
 
   return (
@@ -28,15 +47,21 @@ export default function SignIn() {
         </div>
         <div>
           <h5>Sign in</h5>
-          <form>
-            <input type="text" placeholder="Username"
-              onChange={handleChange("email_id")}
-              value={values.email_id} /> <br />
-            <input type="password" placeholder="Password"
-              onChange={handleChange("password")}
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={handleChange('email_id')}
+              value={values.email_id}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={handleChange('password')}
               value={values.password}
-              onKeyPress={handleEnter} /> <br />
-            <button onClick={() => signinUser(values)}>Continue</button>
+            />
+            <button type="submit">Continue</button>
+            <p className="error-message">{error}</p>
           </form>
         </div>
       </div>
