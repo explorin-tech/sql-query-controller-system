9777d21e6bff03506d8546080ea9bf341b74250b
jai2201 <jai_s@me.iitr.ac.in>
2022-06-18 15:58:06 +0530

    proxy: Remove proxy.

--- Client/src/utils/BackendUrls.js
+++ Client/src/utils/BackendUrls.js
@@ -1,48 +1,68 @@
-export const SIGNIN_URL = '/api/post/login';
-export const GET_ALL_APPLICATIONS = '/api/get/applications';
+export const SIGNIN_URL = 'http://42.204.223.103:1346/api/post/login';
+export const GET_ALL_APPLICATIONS =
+  'http://42.204.223.103:1346/api/get/applications';
 export const GET_ALL_APPLCIATIONS_FOR_AN_USER =
-  '/api/get/applications_for_an_user';
-export const GET_APPLICATION_DETAILS = '/api/get/application_details';
-export const ADD_AN_APPLICATION = '/api/post/application';
-export const EDIT_AN_APPLICATION = '/api/put/application';
-export const DELETE_AN_APPLICATION = '/api/delete/application';
-export const GET_ALL_MAPPED_DATABASES = '/api/get/databases';
-export const GET_DATABASE_DETAILS = '/api/get/database_details';
-export const ADD_DATABASE = '/api/post/database';
-export const EDIT_A_DATABASE = '/api/put/database';
-export const DELETE_A_DATABASE = '/api/delete/database';
-export const GET_ALL_DATABASE_TYPES = '/api/get/database_types';
-export const GET_ALL_USERTYPES = '/api/get/user_types';
-export const GET_ALL_USERS = '/api/get/users';
-export const GET_USER_DETAILS = '/api/get/user_details';
-export const ADD_AN_USER = '/api/post/user';
+  'http://42.204.223.103:1346/api/get/applications_for_an_user';
+export const GET_APPLICATION_DETAILS =
+  'http://42.204.223.103:1346/api/get/application_details';
+export const ADD_AN_APPLICATION =
+  'http://42.204.223.103:1346/api/post/application';
+export const EDIT_AN_APPLICATION =
+  'http://42.204.223.103:1346/api/put/application';
+export const DELETE_AN_APPLICATION =
+  'http://42.204.223.103:1346/api/delete/application';
+export const GET_ALL_MAPPED_DATABASES =
+  'http://42.204.223.103:1346/api/get/databases';
+export const GET_DATABASE_DETAILS =
+  'http://42.204.223.103:1346/api/get/database_details';
+export const ADD_DATABASE = 'http://42.204.223.103:1346/api/post/database';
+export const EDIT_A_DATABASE = 'http://42.204.223.103:1346/api/put/database';
+export const DELETE_A_DATABASE =
+  'http://42.204.223.103:1346/api/delete/database';
+export const GET_ALL_DATABASE_TYPES =
+  'http://42.204.223.103:1346/api/get/database_types';
+export const GET_ALL_USERTYPES =
+  'http://42.204.223.103:1346/api/get/user_types';
+export const GET_ALL_USERS = 'http://42.204.223.103:1346/api/get/users';
+export const GET_USER_DETAILS =
+  'http://42.204.223.103:1346/api/get/user_details';
+export const ADD_AN_USER = 'http://42.204.223.103:1346/api/post/user';
 export const EDIT_USER_DETAILS = '/api/put/user_details';
-export const DELETE_AN_USER = '/api/delete/user';
-export const GET_ALL_APPLICATION_SCREENS = '/api/get/application_screens';
-export const GET_SCREEN_DETIALS = '/api/get/application_screen_details';
-export const ADD_AN_APPLICATION_SCREEN = '/api/post/application_screen';
-export const EDIT_AN_APPLICATION_SCREEN = '/api/put/application_screen';
-export const DELETE_AN_APPLICATION_SCREEN = '/api/delete/application_screen';
+export const DELETE_AN_USER = 'http://42.204.223.103:1346/api/delete/user';
+export const GET_ALL_APPLICATION_SCREENS =
+  'http://42.204.223.103:1346/api/get/application_screens';
+export const GET_SCREEN_DETIALS =
+  'http://42.204.223.103:1346/api/get/application_screen_details';
+export const ADD_AN_APPLICATION_SCREEN =
+  'http://42.204.223.103:1346/api/post/application_screen';
+export const EDIT_AN_APPLICATION_SCREEN =
+  'http://42.204.223.103:1346/api/put/application_screen';
+export const DELETE_AN_APPLICATION_SCREEN =
+  'http://42.204.223.103:1346/api/delete/application_screen';
 export const GET_ALL_SCREEN_RIGHTS_FOR_AN_USER =
-  '/api/get/screen_rights_mapping_for_an_user';
+  'http://42.204.223.103:1346/api/get/screen_rights_mapping_for_an_user';
 export const ADD_SCREEN_RIGHTS_MAPPING_FOR_AN_USER =
-  '/api/post/screen_rights_mapping_for_an_user';
+  'http://42.204.223.103:1346/api/post/screen_rights_mapping_for_an_user';
 export const EDIT_SCREEN_RIGHTS_MAPPING_FOR_AN_USER =
-  '/api/put/screen_rights_mapping_for_an_user';
+  'http://42.204.223.103:1346/api/put/screen_rights_mapping_for_an_user';
 export const GET_ALL_USER_PERMISSION_MAPPING_FOR_AN_USER =
-  '/api/get/user_permission_rights_mappings_for_an_user';
+  'http://42.204.223.103:1346/api/get/user_permission_rights_mappings_for_an_user';
 export const EDIT_USER_PERMISSIONS_MAPPING_FOR_AN_USER =
-  '/api/put/user_permission_rights_mappings_for_an_user';
+  'http://42.204.223.103:1346/api/put/user_permission_rights_mappings_for_an_user';
 export const GET_ALL_MAPPED_DRAFT_QUERIES_FOR_USER =
-  '/api/get/draft_queries_mapped_to_an_user';
+  'http://42.204.223.103:1346/api/get/draft_queries_mapped_to_an_user';
 export const GET_ALL_MAPPED_HISTORY_QUERIES_FOR_USER =
-  '/api/get/history_queries_mapped_to_an_user';
-export const GET_QUERY_DETAILS = '/api/get/query_details';
-export const POST_ADD_NEW_QUERY = '/api/post/query';
+  'http://42.204.223.103:1346/api/get/history_queries_mapped_to_an_user';
+export const GET_QUERY_DETAILS =
+  'http://42.204.223.103:1346/api/get/query_details';
+export const POST_ADD_NEW_QUERY = 'http://42.204.223.103:1346/api/post/query';
 export const EDIT_QUERY_STATUS_FOR_APPROVAL_OR_REJECTION =
-  '/api/put/query_status_for_approval_or_rejection';
-export const EDIT_QUERY = '/api/put/query_in_set_for_approval';
-export const EXECUTE_QUERY = '/api/post/execute_query';
+  'http://42.204.223.103:1346/api/put/query_status_for_approval_or_rejection';
+export const EDIT_QUERY =
+  'http://42.204.223.103:1346/api/put/query_in_set_for_approval';
+export const EXECUTE_QUERY =
+  'http://42.204.223.103:1346/api/post/execute_query';
 export const GET_QUERIES_AWAITING_APPROVAL =
-  '/api/get/queries_awaiting_approval';
-export const GET_RECENT_QUERIES = '/api/get/recent_queries';
+  'http://42.204.223.103:1346/api/get/queries_awaiting_approval';
+export const GET_RECENT_QUERIES =
+  'http://42.204.223.103:1346/api/get/recent_queries';
